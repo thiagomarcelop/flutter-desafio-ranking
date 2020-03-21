afa499d138da4374ba67964d3552f2b0a603018a
Thiago Marcelo Pinto <thiagomarcelopinto@Thiagos-Mac-mini.local>
2020-03-21 17:50:44 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/br/com/thiagomarcelop/flutter_desafio_ranking/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.thiagomarcelop.flutter_desafio_ranking
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
